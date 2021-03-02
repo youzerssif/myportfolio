dbac2cede7175f8d7f7c12cc955ac9dc1a2801fe
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-03-02 17:45:26 +0100

    get

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -146,7 +146,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, '/static'),
     )
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
