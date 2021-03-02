9836ac81c1c6eb4ab426c0dd9f92029b25d5df15
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-03-02 16:54:43 +0100

    get

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -157,6 +157,6 @@ else:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, '../static_cdn')
     MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, '../media_cdn')
 
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
