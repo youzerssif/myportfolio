e5624d46575aec04589ffb1256412e8862241f6c
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-04-11 03:29:06 +0200

    add bd

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -87,19 +87,22 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dd2kncak2rbj5r',
+        'USER': 'dmkstpsozwlwbt',
+        'PASSWORD': '4b532024cf8e4d42c710ac677f970da097e9d870870cdee73f2d6157719209c1',
+        'HOST': 'ec2-3-222-127-167.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'dd2kncak2rbj5r',
-    #     'USER': 'dmkstpsozwlwbt',
-    #     'PASSWORD': '4b532024cf8e4d42c710ac677f970da097e9d870870cdee73f2d6157719209c1',
-    #     'HOST': 'ec2-3-222-127-167.compute-1.amazonaws.com',
-    #     'PORT': '5432',
-    # }
 }
 
 
