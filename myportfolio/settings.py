73595484f7fcaf24f47bfbebd180586c4764ee9c
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-04-11 03:42:54 +0200

    add bd

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -146,6 +146,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     
     # Static files settings
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
